5e528e67e80634f0289943c5eb308e0373313b0a
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-30 16:33:25 +0200

    Calendar picker

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 import streamlit as st
+import datetime
 
 st.title('WaterFlows')
+
+st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
