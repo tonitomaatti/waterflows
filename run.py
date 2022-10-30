b7f12cd907ee482ed85fda5bb832a4fe1035966c
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-31 00:06:22 +0200

    Route Start Markers

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import streamlit as st
 import datetime
 from streamlit_folium import st_folium
 import folium
+import pandas as pd
+import coordinates
 
 st.title('WaterFlows')
 
@@ -10,9 +12,24 @@ with st.sidebar:
 
 m = folium.Map([60.249999, 24.499998], zoom_start=10)
 
+#Draw Lines
+points = [(60.181733310424505, 24.927388429641727), (60.18644904112918, 24.927141666412357)]
+folium.PolyLine(points, color="red", weight=3).add_to(m)
+
+#Add route markers
+df = pd.read_csv("Waterflow Data.csv")
+coords = df[["Route Start GPS N", "Route Start GPS E"]].rename(columns={"Route Start GPS N": "N", "Route Start GPS E": "E"}).to_dict("records")
+
+for coord in coords:
+    coords_wgs84 = coordinates.ETRSTM35FINxy_to_WGS84lalo(coord)
+    folium.Marker(
+        [coords_wgs84["La"], coords_wgs84["Lo"]]
+    ).add_to(m)
+
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width = 1500, height=800)
+st_data = st_folium(m, width = 800)
 
+st.write(st_data)
 
-st.write("Picked Date: ", date_picker)
-st.write("Conda Forge version")
+#st.write("Picked Date: ", date_picker)
+#st.write("Conda Forge version")
