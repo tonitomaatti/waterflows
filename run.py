9e811d39c079bb8c46c537fed26dc3038e99a917
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-30 16:55:12 +0200

    Date picker in sidebar and temp hook

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ import datetime
 
 st.title('WaterFlows')
 
-st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
+with st.sidebar:
+    date_picker = st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
+
+st.write("Picked Date: ", date_picker) 
