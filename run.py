e8113622e3ee778a478db2802c688e1150c3e217
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-30 20:50:01 +0200

    Uusimaa map added

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import datetime
+from streamlit_folium import st_folium
+import folium
 
 st.title('WaterFlows')
 
 with st.sidebar:
     date_picker = st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
+
+m = folium.Map([60.249999, 24.499998], zoom_start=10)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width = 1500, height=800)
+
 
 st.write("Picked Date: ", date_picker)
 st.write("Conda Forge version")
