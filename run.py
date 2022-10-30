576407a8889ddcdbf82f6176ba1e41461959f25e
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-30 19:09:00 +0200

    Testing deployment

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ st.title('WaterFlows')
 with st.sidebar:
     date_picker = st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
 
-st.write("Picked Date: ", date_picker) 
+st.write("Picked Date: ", date_picker)
+st.write("Conda Forge version")
