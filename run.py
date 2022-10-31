b5608d4414769080ea815fc3e757a6f57556d0d2
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-31 03:33:18 +0200

    First route drawn to map

--- run.py
+++ run.py
@@ -5,21 +5,32 @@ import folium
 import pandas as pd
 import coordinates
 from folium.plugins import Draw
+import json
 
 st.title('WaterFlows')
 
 with st.sidebar:
     date_picker = st.date_input("Select Date", min_value=datetime.date.today(), max_value=datetime.date.today()+datetime.timedelta(days=365))
 
-m = folium.Map([60.249999, 24.499998], zoom_start=10)
+m = folium.Map([60.30246404560092, 24.85931396484375], zoom_start=9)
 
 #Draw Lines
 #points = [(60.181733310424505, 24.927388429641727), (60.18644904112918, 24.927141666412357)]
 #folium.PolyLine(points, color="red", weight=3).add_to(m)
 #Draw(export=True).add_to(m)
+with open("Data/Routepoints/routepoints_1.geojson") as f:
+    routepoints_json = json.load(f)
+
+#Add all coordinates together
+routepoints = []
+for points in routepoints_json["features"]:
+    routepoints = routepoints + [(point[1], point[0]) for point in points["geometry"]["coordinates"]]
+
+folium.PolyLine(routepoints, color="red", weight=10).add_to(m)
+
 
 #Add route markers
-df = pd.read_csv("Waterflow Data.csv")
+df = pd.read_csv("Data/Waterflow Data.csv")
 start_coords = df[["Route Start GPS N", "Route Start GPS E", "Route Start"]].rename(
     columns={"Route Start GPS N": "N", "Route Start GPS E": "E", "Route Start": "Name"}
     ).to_dict("records")
