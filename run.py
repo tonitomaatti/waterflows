351094472c527d21253a11d003ee560adc945535
Toni Turpeinen <toni.turpeinen@stat.fi>
2022-10-31 20:00:56 +0200

    coloring with predictions

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ import coordinates
 from folium.plugins import Draw
 import json
 
-
+preds_df = pd.read_csv("Data/year_predictions.csv", sep=",")
+waterflow_df = pd.read_csv("Data/Waterflow Data.csv")
 
 with st.sidebar:
     st.title('WaterFlows')
@@ -26,49 +27,45 @@ else:
 
 m = folium.Map([60.30246404560092, 24.85931396484375], zoom_start=9, tiles=map_tiles)
 
-#Draw Lines
-#points = [(60.181733310424505, 24.927388429641727), (60.18644904112918, 24.927141666412357)]
-#folium.PolyLine(points, color="red", weight=3).add_to(m)
+
 Draw(export=True).add_to(m)
 
-for i in range(1,6):
+day_prediction = preds_df[preds_df["date"]==str(date_picker)]["predicted_mean"].values[0]
+
+routes = []
+for i in range(0,5):
     with open("Data/Routepoints/routepoints_" + str(i)+ ".geojson") as f:
         routepoints_json = json.load(f)
-    folium.GeoJson(routepoints_json, style_function= lambda x: {"color":"#CC3636", "weight":"5"}).add_to(m)
 
-#Add all coordinates together
-#routepoints = []
-#for points in routepoints_json["features"]:
-#    routepoints = routepoints + [(point[1], point[0]) for point in points["geometry"]["coordinates"]]
-#
-#folium.PolyLine(routepoints, color="#CC3636", weight=5).add_to(m)
+    color = "grey"
+    if day_prediction >= waterflow_df.at[i, "Treshold Easy"]:
+        color = "green"
+    elif day_prediction >= waterflow_df.at[i, "Treshold Doable"]:
+        color = "yellow"
+    else:
+        color = "red"
+
+    route = folium.GeoJson(routepoints_json, style_function= lambda x: {"color":color, "weight":"5"}).add_to(m)
+    routes.append(route)
+
+#st.write(routes[0].data)
 
 
 #Add route markers
-df = pd.read_csv("Data/Waterflow Data.csv")
-start_coords = df[["Route Start GPS N", "Route Start GPS E", "Route Start"]].rename(
+
+start_coords = waterflow_df[["Route Start GPS N", "Route Start GPS E", "Route Start"]].rename(
     columns={"Route Start GPS N": "N", "Route Start GPS E": "E", "Route Start": "Name"}
     ).to_dict("records")
 
-end_coords = df[["Route End GPS N", "Route End GPS E", "Route End"]].rename(
+end_coords = waterflow_df[["Route End GPS N", "Route End GPS E", "Route End"]].rename(
     columns={"Route End GPS N": "N", "Route End GPS E": "E", "Route End": "Name"}
     ).to_dict("records")
 
 coords = start_coords + end_coords
 
-#icon = folium.map.Icon(color='red', icon_color='white', icon='info-sign', angle=0, prefix='glyphicon')
-
 for coord in coords:
     route_name = coord.pop("Name", None)
     coords_wgs84 = coordinates.ETRSTM35FINxy_to_WGS84lalo(coord)
-    #folium.Marker(
-    #    [coords_wgs84["La"], coords_wgs84["Lo"]],
-    #    popup=None,
-    #    tooltip=route_name,
-    #    icon=folium.map.Icon(
-    #        color='blue', icon_color='white', icon='fa-flag', angle=0, prefix='fa'
-    #        )
-    #).add_to(m)
     folium.vector_layers.Circle(
         location=(coords_wgs84["La"], coords_wgs84["Lo"]),
         radius=1,
@@ -79,12 +76,13 @@ for coord in coords:
         fill_color="purple",
         fill_opacity=1.0
     ).add_to(m)
-    #TODO: start points green, end points red
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m)
 
-st.write(st_data)
+#if st_data["last_object_clicked"]:
+#    st.write("CLICKED OBJECT")
+
+#st.write(st_data)
 
 #st.write("Picked Date: ", date_picker)
-#st.write("Conda Forge version")
